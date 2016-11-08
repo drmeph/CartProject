c2a88cff59e8f445b18db33297b7715186596bf7
drmeph <kevin.dorfer@gmail.com>
2016-11-07 19:44:07 -0500

    Added security configuration class

---
+++ src/main/java/com/drmeph/cartProject/configuration/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.drmeph.cartProject.configuration;
+
+import com.drmeph.cartProject.security.MySavedRequestAwareAuthenticationSuccessHandler;
+import com.drmeph.cartProject.security.RestAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+/**
+ * Created by kdorfer on 2016-11-05.
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.drmeph.cartProject.security")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private MySavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().
+                withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").authenticated()
+                .and()
+                .formLogin()
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
+                .logout();
+    }
+
+    @Bean
+    public MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler(){
+        return new MySavedRequestAwareAuthenticationSuccessHandler();
+    }
+    @Bean
+    public SimpleUrlAuthenticationFailureHandler myFailureHandler(){
+        return new SimpleUrlAuthenticationFailureHandler();
+    }
+}
