b5fd87648aebf090fad9a054a021d5cb7c5048d8
drmeph <kevin.dorfer@gmail.com>
2016-11-07 20:02:24 -0500

    Added new unit test cases for controllers

---
+++ src/test/java/com/drmeph/cartProject/controller/BaseControllerTest.java
@@ -0,0 +1,33 @@
+package com.drmeph.cartProject.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kdorfer on 2016-11-07.
+ */
+public class BaseControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void setResultCode() throws Exception {
+
+    }
+
+    @Test
+    public void setErrors() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/drmeph/cartProject/controller/CatalogControllerTest.java
@@ -0,0 +1,53 @@
+package com.drmeph.cartProject.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kdorfer on 2016-11-07.
+ */
+public class CatalogControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getListProducts() throws Exception {
+
+    }
+
+    @Test
+    public void showProduct() throws Exception {
+
+    }
+
+    @Test
+    public void rateProduct() throws Exception {
+
+    }
+
+    @Test
+    public void addProduct() throws Exception {
+
+    }
+
+    @Test
+    public void deleteProduct() throws Exception {
+
+    }
+
+    @Test
+    public void getAdminListProduct() throws Exception {
+
+    }
+
+}
