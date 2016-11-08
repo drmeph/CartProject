4b7cd37976102bdbb2d2c81d057903920ead9e37
drmeph <kevin.dorfer@gmail.com>
2016-11-07 20:07:42 -0500

    Added services implementation and tests

---
+++ src/main/java/com/drmeph/cartProject/service/impl/CartServiceImpl.java
@@ -0,0 +1,90 @@
+package com.drmeph.cartProject.service.impl;
+
+import com.drmeph.cartProject.dao.ProductDAO;
+import com.drmeph.cartProject.exception.DAOException;
+import com.drmeph.cartProject.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+
+/**
+ * Created by kdorfer on 2016-11-07.
+ */
+@Service("cartService")
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, value = "session")
+public class CartServiceImpl implements CartService, Serializable {
+    private static final long serialVersionUID = 129498385;
+
+    private HashMap<Integer, Integer> cart;
+
+    @Autowired
+    ProductDAO productDAO;
+
+    @Autowired
+    MessageSource messageSource;
+
+    /**
+     *
+     * @param productId
+     * @throws DAOException
+     */
+    @Override
+    public void addToCart(int productId) throws DAOException {
+
+        if (productDAO.lockUnlockProduct(productId, false)) {
+            addRemoveProductToCart(productId, false);
+        }
+    }
+
+    /**
+     *
+     * @param productId
+     * @throws DAOException
+     */
+    @Override
+    public void removeFromCart(int productId) throws DAOException {
+
+        if (!getCart().containsKey(productId) || getCart().get(productId) <= 0) {
+            throw new DAOException(messageSource.getMessage("noLockedProduct.daoexception.message", null,
+                    Locale.ENGLISH));
+        }
+
+        if (productDAO.lockUnlockProduct(productId, true)) {
+            addRemoveProductToCart(productId, true);
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public HashMap<Integer, Integer> getCart() {
+
+        if (cart == null) {
+            cart = new HashMap<>();
+        }
+
+        return cart;
+    }
+
+    /**
+     *
+     * @param productId
+     */
+    void addRemoveProductToCart(int productId, boolean remove) {
+
+        if (getCart().containsKey(productId)) {
+            getCart().put(productId,
+                    remove ? getCart().get(productId) - 1 : getCart().get(productId) + 1);
+        } else {
+            getCart().put(productId, 1);
+        }
+    }
+}
---
+++ src/test/java/com/drmeph/cartProject/service/impl/CartServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.drmeph.cartProject.service.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kdorfer on 2016-11-07.
+ */
+public class CartServiceImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void addToCart() throws Exception {
+
+    }
+
+    @Test
+    public void removeFromCart() throws Exception {
+
+    }
+
+    @Test
+    public void getCart() throws Exception {
+
+    }
+
+    @Test
+    public void addRemoveProductToCart() throws Exception {
+
+    }
+
+}
